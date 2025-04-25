a9510f148a4ab48f86b28f02b89bc4d761f3e5d8
Phaedra Stemp <pys1000@ron.sr.unh.edu>
2025-04-25 16:54:19 -0400

    updated script

--- final.sh
+++ final.sh
@@ -33,6 +33,7 @@ cd Data_trimmed
 qiime metadata tabulate \
   --m-input-file stats.qza \
   --o-visualization stats.qzv
+#may need solution to exercise four from tutorial#
 
 #information on how many sequences are associated with each sample and with each feature, histograms of those distributions, and some related summary statistics
 
@@ -78,6 +79,7 @@ qiime feature-table summarize-plus \
 
 wget -O 'suboptimal-16S-rRNA-classifier.qza' \
   'https://gut-to-soil-tutorial.readthedocs.io/en/latest/data/gut-to-soil/suboptimal-16S-rRNA-classifier.qza'
+#if things go badly, use different classifier
 
 #classify data using downloaded  program and sorted table
 
@@ -86,26 +88,54 @@ qiime feature-classifier classify-sklearn \
   --i-reads asv-seqs-ms2.qza \
   --o-classification taxonomy.qza
 
-#integrate taxonomy in the sequence summary
-
-#LEFT OFF HERE --- WORKING ON IT (right befor eexeercise 8)
-
-## constructing result collection ##
-rc_name=taxonomy-collection/
-ext=.qza
-keys=( Greengenes-13-8 )
-names=( taxonomy.qza )
-construct_result_collection
-##
-qiime feature-table tabulate-seqs \
-  --i-data asv-seqs-ms2.qza \
-  --i-taxonomy taxonomy-collection/ \
-  --m-metadata-file asv-frequencies-ms2.qza \
-  --o-visualization asv-seqs-ms2.qzv
-
 #Downstream Data Analysis
 #Kmerization of features
 
+#download boots tool from the internet
+conda env create --name q2-boots-amplicon-2025.4 --file https://raw.githubusercontent.com/caporaso-lab/q2-boots/refs/heads/main/environment-files/q2-boots-qiime2-amplicon-2025.4.yml
+source deactivate
+source activate q2-boots-amplicon-2025.4
+
+#move metadata into directory
+cd ..
+mv metadata.tsv Data_trimmed
+cd Data_trimmed
+
 #generate kmer feature table
+#boots provides bootstrapped and rareification-based alpha and beta diversity analyses, designed to mirror the q2-diversity interface
+
+qiime boots kmer-diversity \
+  --i-table asv-table-ms2.qza \
+  --i-sequences asv-seqs-ms2.qza \
+  --m-metadata-file metadata.tsv \
+  --p-sampling-depth 21648 \
+  --p-n 19 \
+  --p-replacement \
+  --p-alpha-average-method median \
+  --p-beta-average-method medoid \
+  --output-dir boots-kmer-diversity
+#use this scatterplot in the report scatter_plot.qzv
+
+#Alpha Rarefication Plottiing
+#alpha rareification plot explore alpha diversity as a function of sampling depth
+#visualizer computes one or more alpha diversity metrics at multiple sampling depths
+
+qiime diversity alpha-rarefaction \
+  --i-table asv-table-ms2.qza \
+  --p-max-depth 62887 \
+  --m-metadata-file metadata.tsv \
+  --o-visualization alpha-rarefaction.qzv
+#use graph in report
+
+#Taxonomic Analysis
+#creates file to show taxonomic composition (bar plots)
+
+qiime taxa barplot \
+  --i-table asv-table-ms2.qza \
+  --i-taxonomy taxonomy.qza \
+  --m-metadata-file metadata.tsv \
+  --o-visualization taxa-bar-plots.qzv
 
 
+#Differential Abundance Testing with ANCOM-BC
+#
